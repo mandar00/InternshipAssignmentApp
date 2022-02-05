b41ce61b8f5bff4b46643b4ccb8038e1454472c4
mandar <sakpalm466@gmail.com>
2022-02-05 19:31:34 +0530

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "./Styles/main.scss"
+import "./Styles/main.css"
 import { BrowserRouter } from 'react-router-dom';
 
 
